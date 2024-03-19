55dbc8b59177c0406ffa0fc21c889fa94072597d
wagnerlm <wagnerlink@gmail.com>
2024-03-19 19:42:26 -0300

    Update

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,6 +1,5 @@
 import json
 import os
-
 import boto3
 
 cognito = boto3.client('cognito-idp')
@@ -12,28 +11,36 @@ cognito_admin_username = os.getenv('COGNITO_ADMIN_USERNAME')
 
 def handler(event, context):
     try:
-        username = json.loads(event['body'])['username']
-        password = json.loads(event['body'])['password']
+        body = json.loads(event['body'])
+        username = body.get('username', cognito_admin_username)
+        password = body['password']
 
-        if username is None or username == '':
-            username = cognito_admin_username
+        user_exists = check_user_exists(username)
 
-        auth_response = sign_up(username, password)
+        if user_exists:
+            auth_response = initiate_auth(username, password)
+        else:
+            auth_response = sign_up(username, password)
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps({
-                'message': 'Usuário autenticado com sucesso',
-                'response': auth_response
-            })
-        }
+        if auth_response:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                    'message': 'Usuário autenticado com sucesso',
+                    'response': auth_response
+                })
+            }
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Não foi possível autenticar o usuário'})
+            }
 
     except cognito.exceptions.NotAuthorizedException:
         return {
             'statusCode': 401,
             'body': json.dumps({'message': 'Usuário não autorizado'})
         }
-
     except Exception as e:
         return {
             'statusCode': 500,
@@ -41,25 +48,31 @@ def handler(event, context):
         }
 
 
-def sign_up(username,password):
+def check_user_exists(username):
+    try:
+        response = cognito.admin_get_user(
+            UserPoolId=cognito_user_pool_id,
+            Username=username
+        )
+        return True if response else False
+    except cognito.exceptions.UserNotFoundException:
+        return False
+    except Exception as e:
+        print("Erro ao verificar existência do usuário:", e)
+        return False
+
+
+def sign_up(username, password):
     try:
         sign_up_response = cognito.sign_up(
             ClientId=cognito_client_id,
             Username=username,
             Password=password
         )
-
-        if sign_up_response['UserConfirmed']:
-            return initiate_auth(username, password)
-        else:
-            return None
-
-    except cognito.exceptions.UsernameExistsException as e:
-        print("Usuário com o mesmo nome de usuário já existe.", e)
-        return initiate_auth(username, password)
+        return initiate_auth(username, password) if sign_up_response else None
     except Exception as e:
-        print("Erro ao criar usuário:", e)
-    return None
+        print("Erro ao registrar usuário:", e)
+        return None
 
 
 def initiate_auth(username, password):
@@ -72,9 +85,6 @@ def initiate_auth(username, password):
             },
             ClientId=cognito_client_id
         )
-    except cognito.exceptions.NotAuthorizedException as e:
-        print("Usuário não autorizado.", e)
-        return None
     except Exception as e:
-        print("Erro ao fazer login:", e)
+        print("Erro ao autenticar usuário:", e)
         return None
