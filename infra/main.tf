e45f33418b86df80ce1f667601273a7b4bfcf8a6
wagnerlm <wagnerlink@gmail.com>
2024-03-19 13:18:47 -0300

    fix name

--- infra/main.tf
+++ infra/main.tf
@@ -2,37 +2,37 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_cognito_user_pools" "mikes-user-pool" {
-  name = "mikes-user-pool"
+data "aws_cognito_user_pools" "point-user-pool" {
+  name = "point-user-pool"
 }
 
-data "aws_cognito_user_pool_clients" "mikes-user-pool-app-client" {
-  user_pool_id = data.aws_cognito_user_pools.mikes-user-pool.ids[0]
+data "aws_cognito_user_pool_clients" "point-user-pool-app-client" {
+  user_pool_id = data.aws_cognito_user_pools.point-user-pool.ids[0]
 }
 
-resource "aws_iam_role" "mikes_lambda_authorizer_role" {
-  name               = "mikes_lambda_authorizer_role"
+resource "aws_iam_role" "point_lambda_authorizer_role" {
+  name               = "point_lambda_authorizer_role"
   assume_role_policy = file("policy/lambda_assume_role_policy.json")
 }
 
-resource "aws_lambda_function" "mikes_lambda_authorizer" {
-  function_name = "mikes_lambda_authorizer"
+resource "aws_lambda_function" "point_lambda_authorizer" {
+  function_name = "point_lambda_authorizer"
   handler       = "app/lambda_function.handler"
   runtime       = "python3.11"
-  role          = aws_iam_role.mikes_lambda_authorizer_role.arn
+  role          = aws_iam_role.point_lambda_authorizer_role.arn
 
   filename = "lambda_function.zip"
 
   source_code_hash = filebase64sha256("lambda_function.zip")
 
   depends_on = [
-    aws_iam_role.mikes_lambda_authorizer_role
+    aws_iam_role.point_lambda_authorizer_role
   ]
 
   environment {
     variables = {
-      COGNITO_CLIENT_ID    = data.aws_cognito_user_pool_clients.mikes-user-pool-app-client.client_ids[0],
-      COGNITO_USER_POOL_ID = data.aws_cognito_user_pools.mikes-user-pool.ids[0],
+      COGNITO_CLIENT_ID    = data.aws_cognito_user_pool_clients.point-user-pool-app-client.client_ids[0],
+      COGNITO_USER_POOL_ID = data.aws_cognito_user_pools.point-user-pool.ids[0],
       COGNITO_ADMIN_USERNAME = var.cognito_admin_username,
     }
   }
